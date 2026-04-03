5deb60b9b0c065a359ac97ce0a8c28fe12f938c8
Tommy Tsang <tommytsang@Tommys-MacBook-Pro.local>
2026-04-03 14:25:28 +0100

    fix: add root layout and globals css

---
+++ src/app/layout.tsx
@@ -0,0 +1,15 @@
+import type { Metadata } from 'next'
+import './globals.css'
+
+export const metadata: Metadata = {
+  title: 'SOON 分鏡指引',
+  description: 'SOON Internal System',
+}
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="zh-HK">
+      <body>{children}</body>
+    </html>
+  )
+}
