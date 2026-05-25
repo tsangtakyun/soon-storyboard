fe1a54d59450d27e080b6cd382694343bd2144b1
Tommy Tsang <tsangtakyun@gmail.com>
2026-05-25 19:39:25 +0100

    Allow storyboard iframe embed

--- next.config.js
+++ next.config.js
@@ -3,6 +3,23 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'X-Frame-Options',
+            value: 'ALLOW-FROM https://egg.sooncreator.network',
+          },
+          {
+            key: 'Content-Security-Policy',
+            value: "frame-ancestors 'self' https://egg.sooncreator.network",
+          },
+        ],
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
